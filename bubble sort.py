8859c8b1e52c194898eb1f55fc66e09ce2af51c2
Xuli2317 <157267645+Xuli2317@users.noreply.github.com>
2024-02-05 16:48:11 +0700

    Update bubble sort.py

--- bubble sort.py
+++ bubble sort.py
@@ -9,7 +9,7 @@ class Renderer(Window):
     def __init__(self):
         super().__init__(1400, 600, "Bubble sort") 
         self.batch = Batch()
-        self.n = [random.randint(1, 150) for _ in range(200)] 
+        self.n = random.sample(range(1, 101), 100) 
         self.bars = self.create_bars()
         self.sort_generator = self.bubble_sort_animation()
         self.sort_complete = False  
@@ -17,11 +17,11 @@ class Renderer(Window):
 
     def create_bars(self):
         bars = []
-        bar_width = 5 
-        bar_spacing = 2  
+        bar_width = 5 # Set the width for each bar
+        bar_spacing = 2  # Set the spacing for each bar
         for i, value in enumerate(self.n):
             bar_height = value * 3  
-            bar_color = (255, 255, 255, 255)  
+            bar_color = (255, 255, 255, 255) # White color for initial bars
             bar = Rectangle(i * (bar_width + bar_spacing), 0, bar_width, bar_height, color=bar_color, batch=self.batch)
             bars.append(bar)
         return bars
@@ -42,7 +42,7 @@ class Renderer(Window):
             self.update_bars_positions(i, j)
         except StopIteration:
             if self.sort_complete:
-                self.change_bars_color((0, 255, 0, 255))  
+                self.change_bars_color((0, 255, 0, 255)) # Change bars to green
             return
 
         clock.schedule_once(lambda dt: self.on_update(dt), self.deltatime)
@@ -50,9 +50,9 @@ class Renderer(Window):
     def update_bars_positions(self, current_i, current_j):
         for i, bar in enumerate(self.bars):
             if i == current_j or i == current_j + 1:
-                bar.color = (255, 0, 0, 255)  
+                bar.color = (255, 0, 0, 255)  # Use different shades of red for sorting bars
             else:
-                bar.color = (255, 255, 255, 255) 
+                bar.color = (255, 255, 255, 255) # Non-sorting bars will be in white color 
             bar.height = self.n[i] * 3  
 
     def change_bars_color(self, color):
@@ -63,6 +63,7 @@ class Renderer(Window):
         self.clear()
         self.batch.draw()
 
+# Create an instance of the Renderer class and run the Pyglet application
 renderer = Renderer()
 pyglet.clock.schedule_once(renderer.on_update, 1.0)
 pyglet.app.run()
